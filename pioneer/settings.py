547f5c5c9e2d943386eb55c0b519a7803f222f47
zakriasuhail <73486892+zakriasuhail@users.noreply.github.com>
2024-07-31 15:43:03 -0700

    confirming backend

--- pioneer/settings.py
+++ pioneer/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'pioneer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'ZakriaReuben123',
+        'HOST': 'localhost',  # Set to your database host, e.g., 'localhost'
+        'PORT': '5432',       # Set to your database port, e.g., '5432'
     }
 }
 
--- signup/models.py
+++ signup/models.py
@@ -1,3 +1,15 @@
+# myapp/models.py
+
 from django.db import models
 
-# Create your models here.
+class Athlete(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)  # Ensure email is unique
+    emergency_contact = models.CharField(max_length=100)  # Adjust max_length as needed
+    dob = models.DateField()  # Date of birth
+    headshot = models.ImageField(upload_to='headshots/')  # Stores image files in 'headshots/' directory
+    height = models.FloatField()  # Use FloatField for height (you can use DecimalField if you need more precision)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
