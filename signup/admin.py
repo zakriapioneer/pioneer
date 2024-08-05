4a2669bd3e21b561438cf1c63a077a129fc9ca4b
zakriapioneer <zakria@gopioneer.io>
2024-08-05 16:02:33 -0700

    added athlete model to admin.py

--- signup/admin.py
+++ signup/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import SignupAthlete
 
 # Register your models here.
+@admin.register(SignupAthlete)
+class SignupAthleteAdmin(admin.AthleteAdmin):
+    list_display = (
+        'id', 
+        'first_name',
+        'last_name',
+        'email',
+        'emergency_contact',
+        'dob',
+        'headshot',
+        'height',
+        'weight',
+        'location',
+        'training_area',
+        'goal',
+        'injury_history',
+        'preference',
+        'equipment'
+    )
--- signup/models.py
+++ signup/models.py
@@ -1,15 +1,208 @@
-# myapp/models.py
-
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-class Athlete(models.Model):
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.SmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
+class SignupAthlete(models.Model):
+    id = models.BigAutoField(primary_key=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)  # Ensure email is unique
-    emergency_contact = models.CharField(max_length=100)  # Adjust max_length as needed
-    dob = models.DateField()  # Date of birth
-    headshot = models.ImageField(upload_to='headshots/')  # Stores image files in 'headshots/' directory
-    height = models.FloatField()  # Use FloatField for height (you can use DecimalField if you need more precision)
+    email = models.CharField(unique=True, max_length=254)
+    emergency_contact = models.CharField(max_length=100)
+    dob = models.DateField()
+    headshot = models.CharField(max_length=100)
+    height = models.FloatField()
+    weight = models.FloatField()
+    location = models.IntegerField()
+    training_area = models.TextField()  # This field type is a guess.
+    goal = models.CharField(blank=True, null=True)
+    injury_history = models.CharField()
+    preference = models.CharField()
+    equipment = models.CharField(blank=True, null=True)
 
-    def __str__(self):
-        return f'{self.first_name} {self.last_name}'
+    class Meta:
+        managed = False
+        db_table = 'signup_athlete'
+
+
+class SignupFacility(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    owner_name = models.CharField()
+    owner_email = models.CharField()
+    owner_phone = models.IntegerField()
+    owner_dob = models.DateField()
+    facility_name = models.CharField(db_column='facility_ name')  # Field renamed to remove unsuitable characters.
+    addresses = models.TextField()  # This field type is a guess.
+    hours = models.TextField()  # This field type is a guess.
+    photos = models.TextField()  # This field type is a guess.
+    videos = models.TextField()  # This field type is a guess.
+    size = models.TextField()  # This field type is a guess.
+    features = models.TextField()  # This field type is a guess.
+    bio = models.CharField()
+    cancellation_policy = models.CharField(blank=True, null=True)
+    trainers = models.TextField(blank=True, null=True)  # This field type is a guess.
+    links = models.TextField(blank=True, null=True)  # This field type is a guess.
+    branding_photos = models.TextField(blank=True, null=True)  # This field type is a guess.
+    training_expertise = models.TextField()  # This field type is a guess.
+
+    class Meta:
+        managed = False
+        db_table = 'signup_facility'
+
+
+class SignupParent(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=254)
+    dob = models.DateField()
+    children_ids = models.TextField()  # This field type is a guess.
+    phone = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'signup_parent'
+
+
+class SignupTrainer(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=254)
+    dob = models.DateField()
+    training_expertise = models.TextField()  # This field type is a guess.
+    headshot = models.BinaryField()
+    actions_pictures = models.TextField(blank=True, null=True)  # This field type is a guess.
+    branding_pictures = models.TextField(blank=True, null=True)  # This field type is a guess.
+    facility_id = models.BigIntegerField(blank=True, null=True)
+    bio = models.TextField()
+    packages = models.TextField()  # This field type is a guess.
+    education = models.TextField()  # This field type is a guess.
+    certifications = models.TextField()  # This field type is a guess.
+    experience = models.CharField()
+    preferences = models.TextField()  # This field type is a guess.
+    links = models.TextField()  # This field type is a guess.
+    cancellation = models.CharField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'signup_trainer'
